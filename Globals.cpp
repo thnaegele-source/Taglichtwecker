7758a34c78b2f93746bc4d74589a4ac0e8240d07
thnaegele-source <th.naegele@googlemail.com>
2026-01-01 10:51:04 +0100

    Update Globals.cpp

--- Globals.cpp
+++ Globals.cpp
@@ -1,41 +1,48 @@
 #include "Globals.h"
 
-// === OBJEKTE DEFINIEREN ===
 TFT_eSPI tft = TFT_eSPI();
-SPIClass touchSPI(HSPI);
+SPIClass touchSPI = SPIClass(VSPI);
 XPT2046_Touchscreen ts(T_CS);
 Adafruit_NeoPixel strip(NUM_LEDS, LED_DATA, NEO_GRB + NEO_KHZ800);
 RTC_DS3231 rtc;
+Preferences preferences;
 
-// === VARIABLEN DEFINIEREN ===
-int16_t touchMinX = 200, touchMaxX = 3900;
-int16_t touchMinY = 200, touchMaxY = 3900;
+int16_t touchMinX = 300, touchMaxX = 3700, touchMinY = 300, touchMaxY = 3700;
 bool lightOn = false;
 unsigned long sunriseStartTime = 0;
+unsigned long screenSunriseStartTime = 0;
 bool sunriseRunning = false;
 bool fastSunrise = false;
 int currentScreen = SCREEN_MAIN;
 unsigned long lastTouchTime = 0;
 unsigned long lastScreenTime = 0;
-bool leftStripOn = false;
-bool rightStripOn = false;
-bool lastLeftState = HIGH;
-bool lastRightState = HIGH;
+bool leftStripOn = false, rightStripOn = false;
+bool lastLeftState = HIGH, lastRightState = HIGH;
+
+// NEU: Dimmen & Doppelklick
+int savedBrightness = 60;
+bool isDimming = false;
+unsigned long dimStartTime = 0;
+int dimDirection = 1;
+unsigned long lastLeftClick = 0;
+unsigned long lastRightClick = 0;
+int leftClickCount = 0;
+int rightClickCount = 0;
 
 AlarmSettings alarmSettings;
 DisplaySettings displaySettings;
 
-const int brightOptions[7] = {15, 20, 30, 45, 60, 90, 120};
+const int brightOptions[7] = {5, 10, 15, 30, 60, 120, 240};
 
 uint32_t sunriseColors[10] = {
-  strip.Color(25, 0, 50),
-  strip.Color(60, 0, 80),
-  strip.Color(100, 0, 60),
-  strip.Color(140, 10, 40),
-  strip.Color(200, 40, 20),
-  strip.Color(240, 90, 10),
-  strip.Color(255, 140, 0),
-  strip.Color(255, 180, 50),
-  strip.Color(255, 210, 100),
-  strip.Color(255, 235, 180)
+  strip.Color(0, 0, 0),
+  strip.Color(10, 0, 0),
+  strip.Color(30, 5, 0),
+  strip.Color(60, 10, 0),
+  strip.Color(100, 20, 0),
+  strip.Color(150, 40, 5),
+  strip.Color(200, 80, 10),
+  strip.Color(255, 120, 20),
+  strip.Color(255, 147, 41),
+  strip.Color(255, 200, 100)
 };
