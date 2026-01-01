4d469cc6d2c8933724ffc2ab95e30771e1b15d16
thnaegele-source <th.naegele@googlemail.com>
2026-01-01 10:51:33 +0100

    Update Global.h

--- Global.h
+++ Global.h
@@ -1,11 +1,11 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
-
 #include <Wire.h>
 #include <RTClib.h>
 #include <TFT_eSPI.h>
 #include <XPT2046_Touchscreen.h>
 #include <Adafruit_NeoPixel.h>
+#include <Preferences.h>
 
 // PINS
 #define TFT_CS     15
@@ -41,11 +41,13 @@ extern SPIClass touchSPI;
 extern XPT2046_Touchscreen ts;
 extern Adafruit_NeoPixel strip;
 extern RTC_DS3231 rtc;
+extern Preferences preferences;
 
 // Variablen
 extern int16_t touchMinX, touchMaxX, touchMinY, touchMaxY;
 extern bool lightOn;
 extern unsigned long sunriseStartTime;
+extern unsigned long screenSunriseStartTime;
 extern bool sunriseRunning;
 extern bool fastSunrise;
 extern int currentScreen;
@@ -53,6 +55,12 @@ extern unsigned long lastTouchTime;
 extern unsigned long lastScreenTime;
 extern bool leftStripOn, rightStripOn;
 extern bool lastLeftState, lastRightState;
+
+// NEU: Dimmen
+extern int savedBrightness;
+extern bool isDimming;
+extern unsigned long dimStartTime;
+extern int dimDirection;
 
 struct AlarmSettings {
   int hour;
@@ -74,20 +82,80 @@ struct DisplaySettings {
 
 extern AlarmSettings alarmSettings;
 extern DisplaySettings displaySettings;
+
 extern const int brightOptions[7];
 extern uint32_t sunriseColors[10];
 
 #define WARM_WHITE strip.Color(255, 147, 41)
 
-// Funktionsdeklarationen
+// Funktionsdeklarationen - Main Screen
 void drawMainScreen();
 void drawLightButton();
 void updateMainScreenTime(DateTime now);
+uint16_t getSkyColor(int hour, int minute, int y);
+uint16_t getGroundColor();
+void getMountainColors(uint16_t &backColor, uint16_t &midColor, uint16_t &frontColor, uint16_t &nearestColor);
+void drawStars();
+void drawStarsWithColor(uint16_t brightColor, uint16_t dimColor);
+void drawMountains();
+
+// Funktionsdeklarationen - Andere Screens
 void drawMenuScreen();
 void drawTimeScreen();
 void drawDateScreen();
 void drawAlarmScreen();
 void drawSunriseScreen(int duration, int brightness);
 void drawDisplayScreen();
+
+// Funktionsdeklarationen - Touch
+void calibrateTouch();
+void loadTouchCalibration();
+void saveTouchCalibration();
+TS_Point waitForTouch();
+
+// Funktionsdeklarationen - Touch Input
+void handleTouchInput(DateTime now);
+void handleButtonPress(int x, int y, DateTime now);
+void handleButtonRepeat(int x, int y);
+void handleDisplayButtons(int x, int y);
+void handleTimeButtons(int x, int y);
+void handleDateButtons(int x, int y);
+void handleAlarmButtons(int x, int y);
+void handleSunriseButtons(int x, int y);
+
+// Sensor State Struct
+struct SensorState {
+  unsigned long pressStartTime;
+  bool wasLongPress;
+  bool isPressed;
+};
+
+// Sensor Data Struct
+struct SensorData {
+  SensorState state;
+  bool isDimming;
+  int dimDirection;
+  unsigned long lastDimUpdate;
+  
+  // Click-Tracking
+  int clickCount;
+  unsigned long firstClickTime;
+  bool actionPending;
+};
+
+// Funktionsdeklarationen - Side Buttons
+void handleSideButtons();
+void handleSensor(bool currState, bool lastState, bool &stripOn, int startLED, int endLED, int darkZone, int fadeZone, const char* side, unsigned long &pressTime, unsigned long &releaseTime, unsigned long &firstReleaseTime, bool &isPressed, bool &wasLongPress, bool &isDimming, int &dimDirection, unsigned long &lastDimUpdate, int &clickCount);
+void setNightlight(int startLED, int endLED, int darkZone, int fadeZone, int brightness);
+void handleBacklight();
+void checkAlarmTrigger();
+
+// Funktionsdeklarationen - Nightlight
+void loadNightlightSettings();
+void saveNightlightSettings();
+void updateDimming();
+
+// Funktionsdeklarationen - Sunrise
+void updateSunrise();
 
 #endif
